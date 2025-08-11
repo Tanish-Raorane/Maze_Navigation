8e1478a435e2ef4530e444f7c59cef6fdf70f45f
Tanish Raorane <140275713+Tanish-Raorane@users.noreply.github.com>
2025-08-11 19:46:11 +0100

    Cane Controller (Still working)

---
+++ Assets/Scripts/CaneController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaneController : MonoBehaviour
+{
+    public GameObject senmagWorkspace;
+    private GameObject stylusCursor;
+    private bool firstTime = false;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+        if (senmagWorkspace.transform.GetChild(0).GetChild(0) == null || senmagWorkspace.transform.childCount == 0)
+        {
+            return;
+        }
+
+        if (!firstTime)
+        {
+            stylusCursor = senmagWorkspace.transform.GetChild(0).GetChild(0).gameObject;
+            firstTime = true;
+        }
+
+    }
+}
